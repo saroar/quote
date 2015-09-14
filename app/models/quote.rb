38ddc4fe4b3a35e4dbe4a6e6c2321923264b23e0
Saroar <saroar9@gmail.com>
2015-09-14 08:24:50 +0000

    Create Quote by rails consule and randon quote on home page

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,10 +1,2 @@
 class Quote < ActiveRecord::Base
-  def create
-    create_table :quotes do |t|
-
-      t.string :saying
-      t.string :author
-
-      t.timestamps
-  end
 end
--- db/migrate/20150914081109_create_quotes.rb
+++ db/migrate/20150914081109_create_quotes.rb
@@ -1,6 +1,8 @@
 class CreateQuotes < ActiveRecord::Migration
   def change
     create_table :quotes do |t|
+      t.string :saying
+      t.string :author
 
       t.timestamps
     end
